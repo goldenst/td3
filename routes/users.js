18ed09faec90a2d7d4b6b2a2e1a29348235c6716
goldenst <jeff@goldenstatetow.com>
2019-06-25 16:34:21 -0700

    setting up dbconnectiion

--- routes/users.js
+++ routes/users.js
@@ -8,4 +8,7 @@ router.post("/", (req, res) => {
   res.send("Register a User");
 });
 
+// User.findAll().then(users => {
+//   console.log("All users:", JSON.stringify(users, null, 4));
+// });
 module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,26 @@
 const express = require("express");
+const Sequelize = require("sequelize");
 
 const app = express();
+
+// Option 1: Passing parameters separately
+const db = new Sequelize("defaultdb", "jwelch", "vc36kby189qbl403", {
+  host: "db-postgresql-nyc3-79966-do-user-6282198-0.db.ondigitalocean.com",
+  dialect: "postgres",
+  port: 25060,
+  ssl: true
+ 
+});
+
+//test db
+db
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
 
 app.get("/", (req, res) => res.json({ msg: "ServerRunning" }));
 
