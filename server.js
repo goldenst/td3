e09adab8cb3d329d226deea45df9b4d8fee4ff83
MSI\jeff <hatcher28@me.com>
2019-06-30 01:07:28 -0700

    prepare for testing deploy

--- server.js
+++ server.js
@@ -1,22 +1,23 @@
 const express = require("express");
-
+const path = require("path");
 
 const app = express();
 
-
 // connect to db
-const db = require ('./config/db')
+const db = require("./config/db");
 //test db
 db.authenticate()
   .then(() => {
-    console.log("Connection has been established successfully to live postgres server.");
+    console.log(
+      "Connection has been established successfully to live postgres server."
+    );
   })
   .catch(err => {
     console.error("Unable to connect to the database:", err);
   });
 
 // Init Middleware
-app.use(express.json({ extended: false }))
+app.use(express.json({ extended: false }));
 
 app.get("/", (req, res) => res.json({ msg: "ServerRunning" }));
 
@@ -25,6 +26,16 @@ app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/employee", require("./routes/employee"));
 app.use("/api/customers", require("./routes/customer"));
+
+// Serve Static assets in Production
+if (process.env.NODE_ENV === "production") {
+  // set Static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
 
 const PORT = process.env.PORT || 5000;
 
